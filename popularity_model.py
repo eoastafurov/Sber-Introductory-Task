8fb5321dc4f2e3557759ec464e4643d584d746ea
Eugene Astafurov <eoastafurov1@gmail.com>
2022-03-07 18:21:17 +0300

    resolved#8

---
+++ popularity_model.py
@@ -0,0 +1,74 @@
+import functools
+import os
+import pickle
+from typing import List
+import pandas as pd
+
+
+class Checkers:
+    @staticmethod
+    def check_fitted(func):
+        @functools.wraps(func)
+        def wrapper_check_fitted(*args, **kwargs):
+            if not isinstance(args[0], PopularityModel):
+                raise RuntimeError('Incorrect usage of this wrapper!')
+            if args[0].popularity_df is None:
+                raise RuntimeError('Call fit(...) first!')
+            return func(*args, **kwargs)
+        return wrapper_check_fitted
+
+
+class PopularityModel:
+    def __init__(self, items_df: pd.DataFrame):
+        self.popularity_df = None
+        self.items_df = items_df
+
+    @Checkers.check_fitted
+    def save(self, path: str):
+        model_dict = {
+            'items_df': self.items_df,
+            'popularity_df': self.popularity_df
+        }
+        with open(path, 'w+b') as f:
+            pickle.dump(model_dict, f)
+        return model_dict
+
+    @staticmethod
+    def load_from_file(path: str):
+        if not os.path.exists(path):
+            raise FileNotFoundError
+
+        with open(path, 'rb') as f:
+            model_dict = pickle.load(f)
+
+        model = PopularityModel(items_df=model_dict['items_df'])
+        model.popularity_df = model_dict['popularity_df']
+
+        model.predict(
+            user_id=None,
+            items_to_ignore=[],
+            topn=1
+        )
+
+        return model
+
+    def fit(self, train_interactions_df: pd.DataFrame):
+        self.popularity_df = train_interactions_df.groupby(
+            'item_id'
+        ).sum().sort_values(
+            by='has_interaction',
+            ascending=False
+        ).reset_index()
+
+        return self
+
+    @Checkers.check_fitted
+    def predict(self, user_id, items_to_ignore: List[int], topn: int):
+        recommendations_df = self.popularity_df[
+            ~self.popularity_df['item_id'].isin(items_to_ignore)
+        ].sort_values(
+            'has_interaction',
+            ascending=False
+        ).head(topn)
+
+        return recommendations_df
